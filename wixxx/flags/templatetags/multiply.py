769214c12e2b9312b9e7f9a695e1b259e2e67a5a
Madison Scott-Clary <madison@archive.org>
2018-12-15 20:40:03 +0000

    Size graph intelligently

--- wixxx/flags/templatetags/multiply.py
+++ wixxx/flags/templatetags/multiply.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter(name='multiply')
 def multiply(x, y):
     return x * y
+
+@register.filter(name='width')
+def width(entry):
+    return entry['count'] * 2 + 10 + len(entry['flag']) * 8
